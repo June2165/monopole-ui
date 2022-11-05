f3b818ec70fe264ef47769d0646ab5c5d307151c
June2165 <99988122+June2165@users.noreply.github.com>
2022-11-05 15:26:07 +0000

    Update index.js

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -7,7 +7,7 @@ const Faq = {
         response: "https://blog.monopole.network",
       },
       {
-        question: "What is Monopole Ecosystem?",
+        question: "What is Monopole Studio?",
         response:
           "Monopole is a platform that makes sustainable projects visible, supporting projects through a studio. Each project will have the possibility to be labelled Proof of Impact",
       },
@@ -17,6 +17,11 @@ const Faq = {
           "The particularity of the Monopole project is that it is on several Blockchains: Elrond, Avalanche, Polygon, Binance Smart Chain, Ethereum",
       },
       {
+        question: "What is Monopole Ecosystem? ",
+        response:
+          "Monopole is a community-driven sustainable finance labelling platform. (S-DAPP)","This labelling makes it possible to make visible projects with a real impact. Measure the impact and affix it to the blockchain so that it is tamper-proof and scorched.","Our signatories are project leaders from several fields of activity."," Our supporters are communities, funds, institutions, investors and our teams and advisors who want to leverage sustainable finance to combine sustainability and profit.","Behind this label, a Startup Studio allowing any project wishing to be accompanied for a Web3.4 project development. One token issue or one funding phase for examples.","We are already in LatAm & Europe.",
+      },
+      {
         question: "Where is the headquarters (HQ) of Monopole?",
         response:
           "Actually, we are on Lisbon. 12.2022: foundation in Switzerland and HQ in LatAm. ",
@@ -24,7 +29,45 @@ const Faq = {
       {
         question: "Why Monopole?",
         response:
-          "Monopole is the only place a maximum decentralise where you have all the view on your portfolio backed on real assets but also Web3 and while making visibility to sustainable finance ",
+          "Monopole is the only place a maximum decentralise where you have all the view on your portfolio backed on real assets but also Web3 and while making visibility to sustainable finance ","The blockchain is a wonderful instrument and starting from its trilemme; Scalability, Security and transparency, we decided to add to this:decentralisation, interoperability and community.","Based on these values, we decided to create a single place, a decentralized maximum where any project achieving a real positive impact could be visible and support.",
+      },
+      {
+        question: "What’s the difference between $MNPL & Monopole? ",
+        response:
+          "Monopole is our platform, $MNPL is our ticker.",
+      },
+      {
+        question: "To whom is Monopole addressed?",
+        response:
+          "Institutions, VCs, Business Angels, Investors and Builders",
+       },
+      {
+        question: "Why did Monopole get its own token?",
+        response:"To give the community an opportunity to have transparent benefits: governance over different strata, access to a network allowing the use of crypto on a daily basis, support for the perennity of our activities, speculation",
+      },
+      {
+        question: "Why such interoperability?",
+        response:
+          "With regard to Mass Adoption and its projections, we want to give visibility to blockchain carrying real values in line with our own. However, a neophyte always arrives through the same channels or blockchain. By making engaged blockchain interoperable, we break down the barrier of complexity and lack of knowledge in favour of engaged blockchain projects",
+      },
+      {
+        question: "When I support a project, do I do it in $MNPL? ",
+        response:
+          "No, Monopole allows the use on any blockchain to support in USDC the projects. The objective is not to have any volatility or low",
+      },
+      {
+        question: "About the tokenomics, where can I find it? ",
+        response:
+          "https://docs.google.com/spreadsheets/d/1ZYD7kZmYHZcgWm0xoYNt2hXaTk0yyZ0O/edit?usp=sharing&ouid=112253211062008925605&rtpof=true&sd=true",
+      },
+      {
+        question: "Regarding the data room, where can I find it? ",
+        response:
+          "https://drive.google.com/drive/folders/1O7Rp-hNMBGYUfgGMH6je3lLVtdmjY7tq?usp=share_link",
+       },
+      {
+        question: "Where to find the social networks of Monopole? ",
+        response:"https://linktr.ee/mnpl",
       },
     ],
   },
@@ -32,7 +75,7 @@ const Faq = {
     category: "Sustainable Finance",
     questions: [
       {
-        question: "What is sustainable finance?",
+        question: "What is Sustainable Finance?",
         response:
           "Finance is the set of mechanisms that provide the economy with the capital it needs to function. It plays a key role in the competitiveness and wealth of a country because its objectives are to ensure the optimal use of resources and thus to improve the well-being of all.But achieving the highest standard of living can only be achieved if the environment and natural resources are taken into account, Financing a socially just ecological transition is therefore a major challenge for the future of our societies. In order to support the real economy and long-term projects, sustainable finance favours financial operations that take into account non-financial criteria called ESG criteria: environmental, social and governance. These criteria include analysis of the impacts of companies activities in terms of carbon emissions, biodiversity protection, waste management, etc.; societal impacts; and the set of rules that govern how businesses are controlled and run.",
       },
@@ -43,8 +86,20 @@ const Faq = {
           "We rely on studies by McKinsey, Bain, Chen Chen and Milton. But also on studies from European Nordic countries. Our entire bibliography will be available on the White Paper",
       },
       {
-        question: "",
-        response: "",
+        question: "Why is it important to consider green finance when making decisions about your investments and purchases? ",
+        response: "When making investments, you should think about green finance for two primary reasons.","1. Financial: since green resources are far more secure than many other sorts of assets in today’s economy.","2. Environmental: green innovations do less damage to the planet than others types of financing or products.",
+      },
+      {
+        question:
+          "What are Monopole’s focus areas in green finance?",
+        response:
+          "We decided to start from the portfolio of diversifications of traditional investors. Will be made visible on Monopole:","- Primary resources & Metal","- Media","- Real Estate","- Web3.4","- Entertainment","- Agriculture","- Climate Tech","- SaaS solutions","...","All projects present on the Studio must first be qualified on the basis of their internal and external commitments.", 
+       },
+      {
+        question:
+          "What is green finance and why is it important?",
+        response:
+          "Green finance refers to types of funding that help reduce a company or consumer’s carbon footprint without breaking the bank. Everyone’s needs and opportunities are different, which is why there are many types of green finance and they are continuously evolving.","This type of financing is important because green projects are generally very capital intensive which means that most new projects would go unfinanced if there was no dedicated funding for development.",
       },
     ],
   },
@@ -52,12 +107,20 @@ const Faq = {
     category: "Proof of Impact Label",
     questions: [
       {
-        question: "What is the Proof of Impact?",
-        response: "",
+        question: "On what basis is the impact at Monopole based?",
+        response: "We used the United Nations Global Goals (SDGs), ISO26000, Lucie26000 and B-corp.","Our goal is to create a sustainable finance model that aligns internal and external values with measured impact."," This impact can be social, economic and/or ecological",
       },
       {
-        question: "",
-        response: "",
+        question: "What countries are you active in?",
+        response: "All countries wishing to participate in this standard and wish to move forward in the long term. ",
+      },
+      {
+        question: "If at qualification, the impact is limited. What help do you provide?",
+        response: "We give access to the Studio for any optimization or project design",
+      },
+      {
+        question: "Is the Proof of Impact Label the sole reflection of Monopole?",
+        response: "We are also working with several committed companies to build this label",
       },
     ],
   },
