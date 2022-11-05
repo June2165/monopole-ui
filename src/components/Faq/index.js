c5b639f508cf8cb8f6e7511be7f6aa4f5be69026
SullFurix <39709633+SullFurix@users.noreply.github.com>
2022-11-05 15:15:54 +0100

    Update index.js

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -8,7 +8,8 @@ const Faq = {
       },
       {
         question: "What is Monopole Ecosystem?",
-        response: "Monopole is a platform that makes sustainable projects visible, supporting projects through a studio. Each project will have the possibility to be labelled Proof of Impact",
+        response:
+          "Monopole is a platform that makes sustainable projects visible, supporting projects through a studio. Each project will have the possibility to be labelled Proof of Impact",
       },
       {
         question: "Based on which Blockchains?",
@@ -24,26 +25,28 @@ const Faq = {
         question: "Why Monopole?",
         response:
           "Monopole is the only place a maximum decentralise where you have all the view on your portfolio backed on real assets but also Web3 and while making visibility to sustainable finance ",
-       }
-    ]
+      },
+    ],
   },
   1: {
     category: "Sustainable Finance",
     questions: [
       {
         question: "What is sustainable finance?",
-        response: "Finance is the set of mechanisms that provide the economy with the capital it needs to function. It plays a key role in the competitiveness and wealth of a country because its objectives are to ensure the optimal use of resources and thus to improve the well-being of all.", "But achieving the highest standard of living can only be achieved if the environment and natural resources are taken into account, Financing a socially just ecological transition is therefore a major challenge for the future of our societies", "In order to support the real economy and long-term projects, sustainable finance favours financial operations that take into account non-financial criteria called ESG criteria: environmental, social and governance." ,"These criteria include analysis of the impacts of companies activities in terms of carbon emissions, biodiversity protection, waste management, etc.; societal impacts; and the set of rules that govern how businesses are controlled and run.",
+        response:
+          "Finance is the set of mechanisms that provide the economy with the capital it needs to function. It plays a key role in the competitiveness and wealth of a country because its objectives are to ensure the optimal use of resources and thus to improve the well-being of all.But achieving the highest standard of living can only be achieved if the environment and natural resources are taken into account, Financing a socially just ecological transition is therefore a major challenge for the future of our societies. In order to support the real economy and long-term projects, sustainable finance favours financial operations that take into account non-financial criteria called ESG criteria: environmental, social and governance. These criteria include analysis of the impacts of companies activities in terms of carbon emissions, biodiversity protection, waste management, etc.; societal impacts; and the set of rules that govern how businesses are controlled and run.",
       },
       {
-        question: "What studies have helped you build Monopole’s geopolitical and financial strategies?",
-        response: "We rely on studies by McKinsey, Bain, Chen Chen and Milton. But also on studies from European Nordic countries. Our entire bibliography will be available on the White Paper",
+        question:
+          "What studies have helped you build Monopole’s geopolitical and financial strategies?",
+        response:
+          "We rely on studies by McKinsey, Bain, Chen Chen and Milton. But also on studies from European Nordic countries. Our entire bibliography will be available on the White Paper",
       },
       {
         question: "",
-        response:
-          "",
-      }
-    ]
+        response: "",
+      },
+    ],
   },
   2: {
     category: "Proof of Impact Label",
@@ -55,9 +58,9 @@ const Faq = {
       {
         question: "",
         response: "",
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
 
 export { Faq };
